9e70878749d81014cc77b2bfdf00b9344844a820
Sikstas Baksenskas <sikstas.baksenskas@gmail.com>
2023-07-09 17:46:24 +0300

    services and tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,4 +17,82 @@ class ActiveSupport::TestCase
 
     ActionController::HttpAuthentication::Basic.encode_credentials(user, password)
   end
+
+  def create_mobile_locations_test_data
+    5.times do |i|
+      MobileLocation.create(
+        lat: 55.1735998,
+        lng: 23.8948016,
+        speed: 0,
+        device_id: 1,
+        driver_id: 1,
+        account_id: 1,
+        time_updated: "2023-07-07T0#{i}:00:00"
+      )
+    end
+
+    1.times do |i|
+      MobileLocation.create(
+        lat: 55.1735998,
+        lng: 23.8948016,
+        speed: 90,
+        device_id: 1,
+        driver_id: 1,
+        account_id: 1,
+        time_updated: "2023-07-07T05:00:00"
+      )
+    end
+
+    3.times do |i|
+      MobileLocation.create(
+        lat: 55.1735998,
+        lng: 23.8948016,
+        speed: 0,
+        device_id: 1,
+        driver_id: 1,
+        account_id: 1,
+        time_updated: "2023-07-07T1#{i}:00:00"
+      )
+    end
+
+    1.times do |i|
+      MobileLocation.create(
+        lat: 55.1735998,
+        lng: 23.8948016,
+        speed: 0,
+        device_id: 1,
+        driver_id: 1,
+        account_id: 1,
+        time_updated: "2023-07-07T13:00:00"
+      )
+    end
+  end
+
+  def create_mobile_location_with_idle_status
+    1.times do |i|
+      MobileLocation.create(
+        lat: 55.1735998,
+        lng: 23.8948016,
+        speed: 0,
+        device_id: 1,
+        driver_id: 1,
+        account_id: 1,
+        time_updated: "2023-07-07T13:00:00"
+      )
+    end
+  end
+
+  def create_mobile_location_with_driving_status
+    1.times do |i|
+      MobileLocation.create(
+        lat: 55.1735998,
+        lng: 23.8948016,
+        speed: 50,
+        device_id: 1,
+        driver_id: 1,
+        account_id: 1,
+        time_updated: "2023-07-07T13:00:00"
+      )
+    end
+  end
 end
